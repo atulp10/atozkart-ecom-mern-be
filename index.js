7faa2c367c26694753d61c2dd6d084c8fe85164b
Atul <atulprjpt@gmail.com>
2025-03-06 17:55:26 +0530

    sameSite:none added

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ app.use(session({
     cookie: {
         httpOnly: true,
         maxAge: 1000 * 60 * 60 * 24 * 7,
-        // sameSite: 'none',
+        sameSite: 'none',
         secure: process.env.NODE_ENV === 'production'
     }
 }))
