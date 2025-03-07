463cf9a70efa5bfbc8de6e589e1d7e4e5a9c3bc4
Atul <atulprjpt@gmail.com>
2025-03-07 11:47:19 +0530

    samesite:none as browser error suggested

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ app.use(session({
         httpOnly: true,
         maxAge: 1000 * 60 * 60 * 24 * 7,
         // sameSite: 'none',
-        // sameSite:(process.env.NODE_ENV === 'production')?'none':'',
+        sameSite:(process.env.NODE_ENV === 'production')?'none':'',
         // secure: process.env.NODE_ENV === 'production'
         secure:false
     }
